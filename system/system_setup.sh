a678b51cb67e45d9a63871c278e5a97d74d5e6c9
Dillon Stadther <dlstadther@gmail.com>
2020-08-18 09:04:58 -0400

    Remove zsh symbolic linking; add change shell

--- system/system_setup.sh
+++ system/system_setup.sh
@@ -34,32 +34,6 @@ else
 fi
 
 # switch to zsh
-# chsh -s `which zsh`
-
-# install configs (back up locations if already exists)
-back_up_if_exists() {
-    SRCDIR=$1
-    NAME=$2
-
-    SRC="$SRCDIR/$NAME"
-    DST="$HOME/$NAME"
-    MODIFIEDDATE=$(date +"%Y%m%dT%H%M%S")
-
-    # if DST exists (either as a dir or a file), suffix with .bak.yyyymmddthhmmss
-    if [[ -d "$DST" -o -f "$DST" ]]; then
-        RENAMED="$DST.bak.$MODIFIEDDATE"
-        echo "Backing up $DST to $RENAMED ..."
-        # mv "$DST" "$RENAMED"
-    fi
-
-    # sym link
-    echo "Symbolic linking $SRC to $DST"
-    # ln -s "$SRC" "$DST"
-}
-
-ZSHDIR="$SCRIPTPATH/zsh"
-back_up_if_exists $ZSHDIR .zsh
-back_up_if_exists $ZSHDIR .zshrc.after
-back_up_if_exists $ZSHDIR .zshrc.before
-back_up_if_exists $ZSHDIR .zshrc
+#echo "Switching default shell to zsh ..."
+chsh -s `which zsh`
 
