2f2386e34f3493fb030636b0bf55ff5a70913766
Dillon Stadther <dlstadther@gmail.com>
2022-04-08 21:08:36 -0400

    Update pyenv init

--- ansible/roles/workstations/files/zsh/.zsh/pyenv.zsh
+++ ansible/roles/workstations/files/zsh/.zsh/pyenv.zsh
@@ -1,4 +1,8 @@
+# set envs
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
 # add 'pyenv init' to shell to enable shims and autocompletion
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
 fi
