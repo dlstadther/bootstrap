b0b525e12e2ff88a6f018e66906d94c316d5851a
Dillon Stadther <dstadther@inmarket.com>
2024-04-09 10:13:54 -0400

    update zsh prompt; move sys time to left

--- ansible/roles/workstations/files/zsh/.zsh/prompt.zsh
+++ ansible/roles/workstations/files/zsh/.zsh/prompt.zsh
@@ -6,6 +6,7 @@ setopt PROMPT_SUBST
 zstyle ':vcs_info:git:*' formats '(%F{green}%r@%b%f) '
 zstyle ':vcs_info:*' enable git
 
+# [%*] - [hh:mm:ss] using 24 hr time
 # %(?.√.?%?) - if return code ? is 0, show √, else show ?%?
 # %? - exit code of previous command
 # %4~ - cwd, shortening HOME to ~, show only last 4 elements
@@ -14,7 +15,8 @@ zstyle ':vcs_info:*' enable git
 # %F{...} - text (foreground) color
 # %f - reset to default textcolor
 # ${vcs_info_msg_0_} - VCS info formatted as specified by zstyle above
-PROMPT='%(?.%F{green}√.%F{red}?%?)%f %B%F{240}%4~%f%b ${vcs_info_msg_0_}%(!.#.>) '
+PROMPT='%F{yellow}[%*] %(?.%F{green}√.%F{red}?%?)%f %B%F{240}%4~%f%b ${vcs_info_msg_0_}%(!.#.>) '
 
 # sys time
-RPROMPT='%*'
+#RPROMPT='%*'
+#RPROMPT=''
