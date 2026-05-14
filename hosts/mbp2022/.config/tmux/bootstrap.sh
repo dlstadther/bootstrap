029e10fef82d32572207c0491bc654c29e09ae12
Dillon Stadther <dlstadther@gmail.com>
2026-05-13 21:29:43 -0400

    chore(mbp2022): add agentsview and middleman tmux sessions to bootstrap

--- hosts/mbp2022/.config/tmux/bootstrap.sh
+++ hosts/mbp2022/.config/tmux/bootstrap.sh
@@ -7,3 +7,15 @@ if ! tmux has-session -t "monitor" 2>/dev/null; then
   tmux new-session -d -s "monitor" -c "$HOME"
   tmux rename-window -t "monitor:1" "sys"
 fi
+
+if ! tmux has-session -t "agentsview" 2>/dev/null; then
+  tmux new-session -d -s "agentsview" -c "$HOME"
+  tmux rename-window -t "agentsview:1" "serve"
+  tmux send-keys -t "agentsview:serve" "agentsview update && agentsview serve" Enter
+fi
+
+if ! tmux has-session -t "middleman" 2>/dev/null; then
+  tmux new-session -d -s "middleman" -c "$HOME/code/middleman"
+  tmux rename-window -t "middleman:1" "serve"
+  tmux send-keys -t "middleman:serve" "git pull origin main && make install && middleman" Enter
+fi
